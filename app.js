8f1f8ac07472a68f586c4822ab1e1a34159e2aad
Artem Danshin <a.danshin@rambler-co.ru>
2017-04-11 20:50:54 +0300

    Organize routes and controllers structure

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-});
+require('./config/routes.js')(app);
 
 app.listen(app.get('port'), () => {
   console.log('Express запущен на http://localhost:' + app.get('port') + ' ; нажмите Ctrl + C для завершения');
