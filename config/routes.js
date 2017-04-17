c46aad384142f6c66ec2904c2060b97348922f17
Artem Danshin <a.danshin@rambler-co.ru>
2017-04-18 00:26:28 +0300

    Add about page in routes

--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,11 @@
 const main      = require('../app/controllers/application');
 const portfolio = require('../app/controllers/portfolio');
 const blog      = require('../app/controllers/blog');
+const about     = require('../app/controllers/about');
 
 module.exports = app => {
   app.get('/', main.home);
   app.get('/portfolio', portfolio.show);
   app.get('/blog', blog.show);
+  app.get('/about', about.show);
 };
