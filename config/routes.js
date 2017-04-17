d6f42750aae3693a2a749b96ce1d1e8acf71758b
Artem Danshin <a.danshin@rambler-co.ru>
2017-04-18 00:24:51 +0300

    Add blog page in routes

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,9 @@
 const main      = require('../app/controllers/application');
 const portfolio = require('../app/controllers/portfolio');
+const blog      = require('../app/controllers/blog');
 
 module.exports = app => {
   app.get('/', main.home);
   app.get('/portfolio', portfolio.show);
+  app.get('/blog', blog.show);
 };
