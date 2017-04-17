f6ff9a0208de32a3531a60f0f2c6c6c9eb89e705
Artem Danshin <a.danshin@rambler-co.ru>
2017-04-18 00:20:22 +0300

    Add portfolio page in routes

--- config/routes.js
+++ config/routes.js
@@ -1,5 +1,7 @@
-const main = require('../app/controllers/application');
+const main      = require('../app/controllers/application');
+const portfolio = require('../app/controllers/portfolio');
 
 module.exports = app => {
   app.get('/', main.home);
+  app.get('/portfolio', portfolio.show);
 };
