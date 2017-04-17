699beb5b358ca87c1394db5f34d0a4d512bfd7dc
Artem Danshin <a.danshin@rambler-co.ru>
2017-04-17 21:10:27 +0300

    Apply manifest files from webpack build

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const path    = require('path');
+const fs      = require('fs');
 
 const app = express();
+const manifest = JSON.parse(fs.readFileSync(path.join(__dirname, 'config', 'manifest.json'), 'utf8'));
 
 app.set('view engine', 'pug');
-app.set('views', path.join(__dirname, 'app/views'));
+app.set('views', path.join(__dirname, 'app', 'views'));
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use((req, res, next) => {
+  res.locals.assets = manifest.assets;
+  next();
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 require('./config/routes.js')(app);
 
