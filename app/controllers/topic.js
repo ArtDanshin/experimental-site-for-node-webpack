167962ecd02b40a299ea2cc5bd86c1fcb930d27f
Artem Danshin <a.danshin@rambler-co.ru>
2017-09-02 18:22:57 +0300

    Add create topic page

--- app/controllers/topic.js
+++ app/controllers/topic.js
@@ -5,10 +5,33 @@ const topicModel = require('../models/topic');
 exports.show = async ctx => {
   const topic = await topicModel.findOne({ slug: ctx.params.slug });
 
+  if (!topic) ctx.throw(404);
+
   await ctx.render('topics', topic);
 };
 
+exports.new = async ctx => {
+  await ctx.render('editor/topics/new')
+};
+
 exports.create = async ctx => {
+  const topic = ctx.request.body;
+
+  await topicModel.create(topic);
+  await ctx.redirect(`/topic/${topic.slug}`);
+};
+
+exports.delete = async ctx => {
+  await ctx.render('editor/topics/new')
+};
+
+exports.update = async ctx => {
+  await ctx.render('editor/topics/new')
+};
+
+
+
+exports.fill = async ctx => {
   const topicFiles = await fs.readdir(path.join(appRoot, 'db', 'topics'));
 
   await topicFiles.map(file => {
