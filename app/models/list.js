d3a78ee40d2a791c17e601afb9ddf990f459c454
Artem Danshin <a.danshin@rambler-co.ru>
2017-09-11 00:51:54 +0300

    Add categories and tags collections. In out mock database to common script

--- app/models/list.js
+++
@@ -1,6 +0,0 @@
-const fs   = require('fs');
-const path = require('path');
-
-module.exports = list => {
-  return JSON.parse(fs.readFileSync(path.join(appRoot, 'db', 'lists', `${list}.json`), 'utf8'));
-};
---
+++ app/models/tag.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const beautifyUnique = require('mongoose-beautiful-unique-validation');
+
+const tagSchema = new mongoose.Schema({
+  title: String,
+  slug: {
+    type: String,
+    unique: true
+  },
+  topics: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Topic'
+  }]
+});
+
+tagSchema.plugin(beautifyUnique);
+
+module.exports = mongoose.model('Tag', tagSchema);
