fb65d61fa9ab4e3c0c3d843066373d13ec2cf991
Artem Danshin <silvir007@gmail.com>
2022-03-28 23:45:16 +0300

    Настроена базовая структура NextJS проекта

---
+++ projects/admin-front/src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next';
+import { Button } from 'antd';
+
+import 'antd/dist/antd.css';
+
+const Home: NextPage = () => {
+  return (
+    <div className="App">
+      <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default Home
