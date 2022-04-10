a0e0d5214d9a955ce0535dba0f510510d4105fd2
Artem Danshin <silvir007@gmail.com>
2022-04-11 00:38:30 +0300

    Внедрен Swagger

--- projects/admin-back/src/main.ts
+++ projects/admin-back/src/main.ts
@@ -1,11 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.setGlobalPrefix('api');
-  await app.listen(6000);
+
+  const config = new DocumentBuilder()
+    .setTitle('ArtDanshin Blog')
+    .setDescription('API админки блога')
+    .setVersion('1.0')
+    .addTag('blog')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-swagger', app, document);
+
+  await app.listen(7000);
 }
 
 bootstrap();
