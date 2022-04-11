db3626b05de782979f2dd1eb110fc67559c2c419
Artem Danshin <silvir007@gmail.com>
2022-04-12 01:04:57 +0300

    Подключена MongoDB

--- projects/admin-back/src/modules/articles/articles.module.ts
+++ projects/admin-back/src/modules/articles/articles.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
+import { TypegooseModule } from 'nestjs-typegoose';
 
 import { ArticlesController } from './articles.controller';
+import { ArticlesModel } from './articles.model';
 
 @Module({
-  controllers: [ArticlesController]
+  controllers: [ArticlesController],
+  imports: [
+    TypegooseModule.forFeature([{
+      typegooseClass: ArticlesModel,
+      schemaOptions: {
+        collection: 'Articles'
+      }
+    }])
+  ]
 })
 export class ArticlesModule {}
